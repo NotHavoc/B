b270f4f28d644b51523ac9e23d01dfbee13808af
NotHavoc <33915989+NotHavoc@users.noreply.github.com>
2017-11-23 19:46:07 -0500

    Update mydudes.py

--- mydudes/mydudes.py
+++ mydudes/mydudes.py
@@ -1,52 +1,18 @@
-import os
 import discord
-import request
 from discord.ext import commands
-from cogs.utils import checks
-from cogs.utils.dataIO import dataIO
-from cogs.utils.chat_formatting import box, pagify, escape_mass_mentions
-#from random import choice
-
-#import time
-#import datetime
 
 class Mydudes:
-    #Cog to notify the server when it is wednesday
+    """My custom cog that does stuff!"""
 
     def __init__(self, bot):
         self.bot = bot
-        #self.triggered = false 
-        
-    async def on_message(self, message):
-        #reads server messages, if it is the first message of wednesday posts the notification
-        channel = message.channel
-        author = message.author
-       # weekday = datetime.now().isoweekday()
-        weekday = 3
 
-        if message.server is None:
-            return
+    @commands.command()
+    async def mycom(self):
+        """This does stuff!"""
 
-        if author == self.bot.user:
-            return
-        
-        if not self.bot.user_allowed(message):
-            return
+        #Your code will go here
+        await self.bot.say("It is Wednesday my dudes.")
 
-        if self.is_command(message):
-            return
-        
-        if (weekday != 3):
-        #    self.tiggered = false
-            return
-
-        #if (triggered == true):
-         #   return
-      
-        await self.bot.say("It is Wednesday my dudes")
-        triggered = true
-
-    def setup(bot):
-        bot.add_cog(Mydudes(bot))
-
-
+def setup(bot):
+    bot.add_cog(MYdudes(bot))
