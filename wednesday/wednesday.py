a73687d3e58405099458320e7d222d236d8ccd4c
NotHavoc <33915989+NotHavoc@users.noreply.github.com>
2017-11-23 19:14:51 -0500

    Update wednesday.py

--- wednesday/wednesday.py
+++ wednesday/wednesday.py
@@ -5,13 +5,49 @@ from discord.ext import commands
 from cogs.utils import checks
 from cogs.utils.dataIO import dataIO
 from cogs.utils.chat_formatting import box, pagify, escape_mass_mentions
-#from random import choice
+from random import choice
 
 #import time
 #import datetime
 
 class Wednesday:
     """Cog to notify the server when it is wednesday"""
+
+    def init(self, bot):
+        self.bot = bot
+        #self.triggered = false 
+
+    def on_message(message):
+        #reads server messages, if it is the first message of wednesday posts the notification
+        channel = message.channel
+        author = message.author
+        weekday = 3
+        #datetime.now().isoweekday()
+
+        if(weekday == 3)
+            await self.bot.say("It is Wednesday my dudes")
+
+        else
+            return
+
+    def setup(bot):
+        bot.add_cog(Wednesday(bot))
+
+"""
+import os
+import discord
+import request
+from discord.ext import commands
+from cogs.utils import checks
+from cogs.utils.dataIO import dataIO
+from cogs.utils.chat_formatting import box, pagify, escape_mass_mentions
+#from random import choice
+
+#import time
+#import datetime
+
+class Wednesday:
+    #Cog to notify the server when it is wednesday
 
     def __init__(self, bot):
         self.bot = bot
@@ -29,13 +65,13 @@ class Wednesday:
 
         if author == self.bot.user:
             return
-        """
+        
         if not self.bot.user_allowed(message):
             return
 
         if self.is_command(message):
             return
-        """
+        
         if (weekday != 3):
         #    self.tiggered = false
             return
@@ -49,3 +85,4 @@ class Wednesday:
     def setup(bot):
         bot.add_cog(Wednesday(bot))
 
+"""
