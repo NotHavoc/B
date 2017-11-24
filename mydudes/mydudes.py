e2303d7653767a7bec203da81db80218abbde44f
NotHavoc <33915989+NotHavoc@users.noreply.github.com>
2017-11-23 19:23:14 -0500

    Add files via upload

---
+++ mydudes/mydudes.py
@@ -0,0 +1,52 @@
+import os
+import discord
+import request
+from discord.ext import commands
+from cogs.utils import checks
+from cogs.utils.dataIO import dataIO
+from cogs.utils.chat_formatting import box, pagify, escape_mass_mentions
+#from random import choice
+
+#import time
+#import datetime
+
+class Mydudes:
+    #Cog to notify the server when it is wednesday
+
+    def __init__(self, bot):
+        self.bot = bot
+        #self.triggered = false 
+        
+    async def on_message(self, message):
+        #reads server messages, if it is the first message of wednesday posts the notification
+        channel = message.channel
+        author = message.author
+       # weekday = datetime.now().isoweekday()
+        weekday = 3
+
+        if message.server is None:
+            return
+
+        if author == self.bot.user:
+            return
+        
+        if not self.bot.user_allowed(message):
+            return
+
+        if self.is_command(message):
+            return
+        
+        if (weekday != 3):
+        #    self.tiggered = false
+            return
+
+        #if (triggered == true):
+         #   return
+      
+        await self.bot.say("It is Wednesday my dudes")
+        triggered = true
+
+    def setup(bot):
+        bot.add_cog(Mydudes(bot))
+
+
